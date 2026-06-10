f249381c2f009ec1805c471b77607e59144cd116
Brad Preston <bap5393@gmail.com>
2026-06-10 15:00:00 -0400

    configure terraform providers

---
+++ infrastructure/shared/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+      digitalocean = {
+        source = "digitalocean/digitalocean",
+        version = "~> 2.0"
+      }
+      cloudflare = {
+        source = "cloudflare/cloudflare",
+        version = "~> 4.0"
+      }
+    }
+}
+
+variable "do_token" {}
+variable "cf_api_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+provider "cloudflare" {
+  api_token = var.cf_api_token
+}
